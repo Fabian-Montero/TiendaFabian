d2038a5fc34870c173f53b1a5a9beb76b20ee10e
Fabian-Montero <fabianja0477@gmail.com>
2023-01-23 21:07:33 -0600

    Primer commit

---
+++ src/main/java/com/TiendaFabian/demo/TiendaFabianApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaFabian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFabianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFabianApplication.class, args);
+	}
+
+}
